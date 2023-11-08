de9812ef2b465eeb58fce4b33c840196639b27b3
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-11-08 14:46:15 +0000

    refactor: use template class, remove threads

--- docs/presentation/examples/gauss/main.cpp
+++ docs/presentation/examples/gauss/main.cpp
@@ -1,72 +1,89 @@
-#include <getopt.h>
 #include <gmpxx.h>
 
 #include <chrono>
 #include <fstream>
-#include <future>
 #include <iostream>
-#include <thread>
-#include <typeinfo>
 
 #include "gauss_fp.h"
 #include "gauss_np.h"
 #include "gauss_pp.h"
 
-#define EPS 1e-13
-
-std::mutex mtx;  // mutex for critical section
-
 template <template <class> class G, class T>
-std::unique_ptr<T[]> benchmark(size_t size, size_t seed, std::ostream& output) {
-  static_assert(std::is_same<T, double>::value || std::is_same<T, mpq_class>::value,
-                "T must be either double or mpq_class");
-  static_assert(std::is_base_of<dlinear::Gauss<T>, G<T>>::value, "G must be a subclass of Gauss<T>");
-  std::unique_ptr<G<T>> gauss = std::make_unique<G<T>>(size, seed);
-  mtx.lock();
-  gauss->random_generate();
-  mtx.unlock();
+class GaussBenchmark {
+ public:
+  GaussBenchmark(std::ostream& timing_, std::ostream& diff, size_t size, size_t seed)
+      : timing_{timing_}, diff_{diff}, gauss_{std::move(std::make_unique<G<T>>(size, seed))}, x_{} {}
 
-  std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-  std::unique_ptr<T[]> x{gauss->solve()};
-  std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-  auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+  void benchmark() {
+    static_assert(std::is_same<T, double>() || std::is_same<T, mpq_class>() || std::is_same<T, mpf_class>(),
+                  "T must be either double or mpq_class");
+    static_assert(std::is_base_of<dlinear::Gauss<T>, G<T>>(), "G must be a subclass of Gauss<T>");
+    gauss_->random_generate();
 
-  mtx.lock();
-  output << gauss->class_name() << "," << gauss->type_name() << "," << time << "," << gauss->seed() << ","
-         << gauss->size() << std::endl;
-  mtx.unlock();
-  return x;
-}
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    x_ = std::move(gauss_->solve());
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
 
-inline double get_double(const double& x) { return x; }
-inline double get_double(const mpq_class& x) { return x.get_d(); }
-
-template <class L, class R>
-double average_difference(const L x[], const R y[], size_t size) {
-  double sum = 0;
-  for (size_t i = 0; i < size; ++i) {
-    sum += std::abs(get_double(x[i]) - get_double(y[i]));
+    timing_ << gauss_->class_name() << "," << gauss_->type_name() << "," << time << "," << gauss_->seed() << ","
+            << gauss_->size() << std::endl;
   }
-  return sum / size;
-}
 
-template <class L, class R>
-double abs_difference(const L x[], const R y[], size_t size) {
-  double sum = 0;
-  for (size_t i = 0; i < size; ++i) {
-    sum += std::abs(get_double(x[i]) - get_double(y[i]));
-  }
-  return sum;
-}
+  inline double get_double(const double& x) const { return x; }
+  inline double get_double(const mpq_class& x) const { return x.get_d(); }
+  inline double get_double(const mpf_class& x) const { return x.get_d(); }
 
-template <class L, class R>
-double difference(const L x[], const R y[], size_t size) {
-  double sum = 0;
-  for (size_t i = 0; i < size; ++i) {
-    sum += get_double(x[i]) - get_double(y[i]);
+  template <template <class> class O, class OT>
+  double diff(const GaussBenchmark<O, OT>& o) const {
+    ensure_same_size(o);
+    double sum = 0;
+    for (size_t i = 0; i < gauss_->size(); ++i) {
+      sum += get_double(x_[i]) - get_double(o.x()[i]);
+    }
+    return sum;
   }
-  return sum;
-}
+
+  template <template <class> class O, class OT>
+  double asb_diff(const GaussBenchmark<O, OT>& o) const {
+    ensure_same_size(o);
+    double sum = 0;
+    for (size_t i = 0; i < gauss_->size(); ++i) {
+      sum += std::abs(get_double(x_[i]) - get_double(o.x()[i]));
+    }
+    return sum;
+  }
+
+  template <template <class> class O, class OT>
+  double square_mean(const GaussBenchmark<O, OT>& o) const {
+    ensure_same_size(o);
+    double sum = 0;
+    for (size_t i = 0; i < gauss_->size(); ++i) {
+      sum += std::abs(get_double(x_[i]) - get_double(o.x()[i]));
+    }
+    return sum / gauss_->size();
+  }
+
+  template <template <class> class O, class OT>
+  void print_compare_to_baseline(const GaussBenchmark<O, OT>& baseline) const {
+    ensure_same_size(baseline);
+    diff_ << gauss_->class_name() << "," << gauss_->type_name() << "," << gauss_->size() << "," << square_mean(baseline)
+          << "," << asb_diff(baseline) << "," << diff(baseline) << std::endl;
+  }
+
+  inline const std::unique_ptr<T[]>& x() const { return x_; }
+  inline size_t size() const { return gauss_->size(); }
+
+ private:
+  template <template <class> class O, class OT>
+  void ensure_same_size(const GaussBenchmark<O, OT>& o) const {
+    if (gauss_->size() != o.size()) throw std::runtime_error("Cannot compare different sizes");
+  }
+
+  std::ostream& timing_;
+  std::ostream& diff_;
+  std::unique_ptr<G<T>> gauss_;
+  std::unique_ptr<T[]> x_;
+};
 
 /**
  * @brief Usage:
@@ -74,7 +91,7 @@ double difference(const L x[], const R y[], size_t size) {
  */
 int main(int argc, char const* argv[]) {
   size_t size = 10, seed = 42;
-  const char *output_file = "gauss.csv", *diff_file = "diff.csv";
+  const char *output_file = "time.csv", *diff_file = "diff.csv";
   if (argc >= 2 && std::atoi(argv[1]) > 0) {
     size = std::atoi(argv[1]);
   }
@@ -95,36 +112,34 @@ int main(int argc, char const* argv[]) {
   }
   output << "solver,type,time,seed,size" << std::endl;
 
-  auto f1 = std::async(benchmark<dlinear::GaussNP, double>, size, seed, std::ref(output));
-  auto f2 = std::async(benchmark<dlinear::GaussPP, double>, size, seed, std::ref(output));
-  auto f3 = std::async(benchmark<dlinear::GaussFP, double>, size, seed, std::ref(output));
-  auto f4 = std::async(benchmark<dlinear::GaussNP, mpq_class>, size, seed, std::ref(output));
-  auto f5 = std::async(benchmark<dlinear::GaussPP, mpq_class>, size, seed, std::ref(output));
-  auto f6 = std::async(benchmark<dlinear::GaussFP, mpq_class>, size, seed, std::ref(output));
+  GaussBenchmark<dlinear::GaussNP, double> g1{output, diff, size, seed};
+  GaussBenchmark<dlinear::GaussPP, double> g2{output, diff, size, seed};
+  GaussBenchmark<dlinear::GaussFP, double> g3{output, diff, size, seed};
+  GaussBenchmark<dlinear::GaussNP, mpq_class> g4{output, diff, size, seed};
+  GaussBenchmark<dlinear::GaussPP, mpq_class> g5{output, diff, size, seed};
+  GaussBenchmark<dlinear::GaussFP, mpq_class> g6{output, diff, size, seed};
+  GaussBenchmark<dlinear::GaussNP, mpf_class> g7{output, diff, size, seed};
+  GaussBenchmark<dlinear::GaussPP, mpf_class> g8{output, diff, size, seed};
+  GaussBenchmark<dlinear::GaussFP, mpf_class> g9{output, diff, size, seed};
 
-  auto x1 = f1.get();
-  auto x2 = f2.get();
-  auto x3 = f3.get();
-  auto x4 = f4.get();
-  auto x5 = f5.get();
-  auto x6 = f6.get();
+  g1.benchmark();
+  g2.benchmark();
+  g3.benchmark();
+  g4.benchmark();
+  g5.benchmark();
+  g6.benchmark();
+  g7.benchmark();
+  g8.benchmark();
+  g9.benchmark();
 
-  diff << "solver,type,size,avg,abs,diff" << std::endl;
-  diff << "GaussNP,double," << size << "," << average_difference(x1.get(), x6.get(), size) << ","
-       << abs_difference(x1.get(), x6.get(), size) << "," << difference(x1.get(), x6.get(), size) << std::endl;
-  diff << "GaussPP,double," << size << "," << average_difference(x2.get(), x6.get(), size) << ","
-       << abs_difference(x2.get(), x6.get(), size) << "," << difference(x2.get(), x6.get(), size) << std::endl;
-  diff << "GaussFP,double," << size << "," << average_difference(x3.get(), x6.get(), size) << ","
-       << abs_difference(x3.get(), x6.get(), size) << "," << difference(x3.get(), x6.get(), size) << std::endl;
-  diff << "GaussNP,mpq_class," << size << "," << average_difference(x4.get(), x6.get(), size) << ","
-       << abs_difference(x4.get(), x6.get(), size) << "," << difference(x4.get(), x6.get(), size) << std::endl;
-  diff << "GaussPP,mpq_class," << size << "," << average_difference(x5.get(), x6.get(), size) << ","
-       << abs_difference(x5.get(), x6.get(), size) << "," << difference(x5.get(), x6.get(), size) << std::endl;
-  diff << "GaussFP,mpq_class," << size << "," << average_difference(x6.get(), x6.get(), size) << ","
-       << abs_difference(x6.get(), x6.get(), size) << "," << difference(x6.get(), x6.get(), size) << std::endl;
+  g1.print_compare_to_baseline(g6);
+  g2.print_compare_to_baseline(g6);
+  g3.print_compare_to_baseline(g6);
+  g4.print_compare_to_baseline(g6);
+  g5.print_compare_to_baseline(g6);
+  g7.print_compare_to_baseline(g6);
+  g8.print_compare_to_baseline(g6);
+  g9.print_compare_to_baseline(g6);
 
   return 0;
 }
-
-// 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35 37 39 41 43 45 47 49 51 53 55 57 59 61 63 65 67 69 71 73 75 77 79 81
-// 83 85 87 89 91 93 95 97 99
