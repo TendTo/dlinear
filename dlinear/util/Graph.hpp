5fef57516652de29096f105364e1c07d7be5a047
Ernesto Casablanca <casablancaernesto@gmail.com>
2024-03-15 18:28:39 +0000

    docs: update doctrings for Graph

--- dlinear/util/Graph.hpp
+++ dlinear/util/Graph.hpp
@@ -1,45 +1,107 @@
+/**
+ * @file Graph.hpp
+ * @author dlinear (https://github.com/TendTo/dlinear)
+ * @copyright 2024 dlinear
+ * @licence Apache-2.0 license
+ * @brief Graph class.
+ *
+ * Generic graph implementation that can be used to represent a graph with vertices of type @p T.
+ */
+#pragma once
+
 #include <functional>
 #include <iostream>
 #include <queue>
 #include <stack>
 #include <unordered_map>
 #include <unordered_set>
-#include <vector>
 
 namespace dlinear {
 
+/**
+ * Graph class.
+ *
+ * Generic implementation that can be used to represent a graph with vertices of type @p T.
+ * @tparam T element type of the vertices
+ */
 template <class T>
 class Graph {
  public:
-  // Constructor not needed if you don't have any initializations different from the default ones
-
-  void AddEdge(const T& u, const T& v, bool bidir = true) {
+  /**
+   * Add an edge to the graph from vertex @p u to vertex @p v
+   * @param u from vertex
+   * @param v to vertex
+   * @param bidirectional whether to add another edge from @p v to @p u
+   */
+  void AddEdge(const T& u, const T& v, bool bidirectional = true) {
     adj_list_[u].insert(v);
-    if (bidir) adj_list_[v].insert(u);
+    if (bidirectional) adj_list_[v].insert(u);
   }
 
+  /**
+   * Add a vertex @p v to the graph.
+   *
+   * The edge set for the vertex is initialized to an empty set.
+   * @param v vertex to add
+   */
   void AddVertex(const T& v) {
     if (adj_list_.find(v) == adj_list_.end()) adj_list_[v] = std::unordered_set<T>();
   }
 
+  /**
+   * Check if the graph contains an edge from vertex @p u to vertex @p v
+   * @param u from vertex
+   * @param v to vertex
+   * @return true if the graph contains an edge from @p u to @p v
+   * @return false if either @p u or @p v is not in the graph or if there is no edge from @p u to @p v
+   */
   bool HasEdge(const T& u, const T& v) const {
     return adj_list_.find(u) != adj_list_.cend() && adj_list_.at(u).find(v) != adj_list_.at(u).cend();
   }
 
+  /**
+   * Return the adjacency list of the graph
+   * @return a map from vertex to the set of vertices that are adjacent to it
+   */
   const std::unordered_map<T, std::unordered_set<T>>& adj_list() const { return adj_list_; }
 
-  void RemoveEdge(const T& u, const T& v, bool bidir = true) {
+  /**
+   * Remove an edge from vertex @p u to vertex @p v
+   * @param u from vertex
+   * @param v to vertex
+   * @param bidirectional whether to remove the edge from @p v to @p u too
+   */
+  void RemoveEdge(const T& u, const T& v, bool bidirectional = true) {
     adj_list_[u].erase(v);
-    if (bidir) adj_list_[v].erase(u);
+    if (bidirectional) adj_list_[v].erase(u);
   }
 
+  /**
+   * Remove a vertex @p v from the graph
+   * @param v vertex to remove
+   */
   void RemoveVertex(const T& v) {
     adj_list_.erase(v);
     for (auto& [node, edges] : adj_list_) edges.erase(v);
   }
 
+  /** Clear the graph, removing all vertices and edges. */
+  void Clear() { adj_list_.clear(); }
+
+  /**
+   * Check if the graph is empty
+   * @return true if the graph is empty
+   * @return false if the graph is not empty
+   */
   [[nodiscard]] bool IsEmpty() const { return adj_list_.empty(); }
 
+  /**
+   * Explore the graph using depth-first search starting from vertex @p start.
+   *
+   * Each vertex is visited exactly once and the function @p func is called on each one.
+   * @param start starting vertex
+   * @param func function to call on each vertex upon visiting it
+   */
   void DFS(const T& start, const std::function<void(const T&)>& func) {
     std::unordered_set<T> visited{};
     visited.reserve(adj_list_.size());
@@ -60,6 +122,13 @@ class Graph {
     }
   }
 
+  /**
+   * Explore the graph using breadth-first search starting from vertex @p start.
+   *
+   * Each vertex is visited exactly once and the function @p func is called on each one.
+   * @param start starting vertex
+   * @param func function to call on each vertex upon visiting it
+   */
   void BFS(const T& start, const std::function<void(const T&)>& func) {
     std::unordered_set<T> visited{};
     visited.reserve(adj_list_.size());
@@ -78,7 +147,7 @@ class Graph {
   }
 
  private:
-  std::unordered_map<T, std::unordered_set<T>> adj_list_;
+  std::unordered_map<T, std::unordered_set<T>> adj_list_;  ///< Adjacency list of the graph
 };
 
 template <typename T>
