a33030ca473528b1743fbdd3d0b6549e98b1b772
Ernesto Casablanca <casablancaernesto@gmail.com>
2024-04-17 15:48:17 +0100

    feat: add custom function definitions for smt2

---
+++ dlinear/smt2/FunctionDefinition.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file FunctionDefinition.cpp
+ * @author dlinear (https://github.com/TendTo/dlinear)
+ * @copyright 2024 dlinear
+ * @licence Apache-2.0 license
+ */
+#include "FunctionDefinition.h"
+
+#include <utility>
+
+namespace dlinear::smt2 {
+
+FunctionDefinition::FunctionDefinition(std::vector<Variable> parameters, Sort return_type, Term body)
+    : parameters_{std::move(parameters)}, return_type_{return_type}, body_{std::move(body)} {
+  body_.Check(return_type_);
+}
+
+Term FunctionDefinition::operator()(const std::vector<Term>& arguments) const {
+  if (parameters_.size() != arguments.size()) {
+    DLINEAR_RUNTIME_ERROR_FMT("This function definition expects #{} arguments ({}). Provided #{} arguments.",
+                              parameters_.size(), parameters_, arguments.size());
+  }
+  Term t = body_;
+
+  for (size_t i = 0; i < parameters_.size(); ++i) {
+    const Variable& param_i{parameters_[i]};
+    const Term& arg_i{arguments[i]};
+    arg_i.Check(param_i.get_type());
+    t = t.Substitute(param_i, arg_i);
+  }
+
+  return t;
+}
+
+}  // namespace dlinear::smt2
---
+++ test/smt2/TestFunctionDefinition.cpp
@@ -0,0 +1,76 @@
+/**
+ * @file TestFunctionDefinition.cpp
+ * @author dlinear (https://github.com/TendTo/dlinear)
+ * @copyright 2024 dlinear
+ * @licence Apache-2.0 license
+ */
+#include <gtest/gtest.h>
+
+#include "dlinear/smt2/FunctionDefinition.h"
+#include "test/symbolic/TestSymbolicUtils.h"
+
+using dlinear::Expression;
+using dlinear::Variable;
+using dlinear::smt2::FunctionDefinition;
+using dlinear::smt2::ParseSort;
+using dlinear::smt2::Sort;
+using dlinear::smt2::SortToType;
+using dlinear::smt2::Term;
+using std::runtime_error;
+
+class TestFunctionDefinition : public ::testing::TestWithParam<bool> {
+  const DrakeSymbolicGuard guard_;
+
+ protected:
+  const double min_val_ = 5, max_val_ = 6;
+  const Variable lhs_{"lhs"}, rhs_{"rhs"};
+  const Variable x_{"x"}, y_{"y"}, z_{"z"};
+  const double a_val_ = 2, b_val_ = 3, c_val_ = 5;
+};
+
+INSTANTIATE_TEST_SUITE_P(TestFunctionDefinition, TestFunctionDefinition, ::testing::Values(false, true));
+
+TEST_P(TestFunctionDefinition, MaxFunctionDefinition) {
+  const Expression body = if_then_else(lhs_ > rhs_, lhs_, rhs_);
+  const FunctionDefinition fun{{lhs_, rhs_}, Sort::Real, Term{body}};
+
+  const auto res = GetParam() ? fun(Term{min_val_}, Term{max_val_}) : fun({Term{min_val_}, Term{max_val_}});
+
+  EXPECT_EQ(res.type(), Term::Type::EXPRESSION);
+  EXPECT_TRUE(is_constant(res.expression()));
+  EXPECT_EQ(get_constant_value(res.expression()), std::max(min_val_, max_val_));
+}
+
+TEST_P(TestFunctionDefinition, MinFunctionDefinition) {
+  const Expression body = if_then_else(lhs_ < rhs_, lhs_, rhs_);
+  const FunctionDefinition fun{{lhs_, rhs_}, Sort::Real, Term{body}};
+
+  const Term res = GetParam() ? fun(Term{min_val_}, Term{max_val_}) : fun({Term{min_val_}, Term{max_val_}});
+
+  EXPECT_EQ(res.type(), Term::Type::EXPRESSION);
+  EXPECT_TRUE(is_constant(res.expression()));
+  EXPECT_EQ(get_constant_value(res.expression()), std::min(min_val_, max_val_));
+}
+
+TEST_P(TestFunctionDefinition, SumFunctionDefinition) {
+  const Expression body = lhs_ + rhs_;
+  const FunctionDefinition fun{{lhs_, rhs_}, Sort::Real, Term{body}};
+
+  const Term res = GetParam() ? fun(Term{min_val_}, Term{max_val_}) : fun({Term{min_val_}, Term{max_val_}});
+
+  EXPECT_EQ(res.type(), Term::Type::EXPRESSION);
+  EXPECT_TRUE(is_constant(res.expression()));
+  EXPECT_EQ(get_constant_value(res.expression()), min_val_ + max_val_);
+}
+
+TEST_P(TestFunctionDefinition, SubFunctionDefinition) {
+  const Expression body = x_ - y_ - z_;
+  const FunctionDefinition fun{{x_, y_, z_}, Sort::Real, Term{body}};
+
+  const Term res =
+      GetParam() ? fun(Term{a_val_}, Term{b_val_}, Term{c_val_}) : fun({Term{a_val_}, Term{b_val_}, Term{c_val_}});
+
+  EXPECT_EQ(res.type(), Term::Type::EXPRESSION);
+  EXPECT_TRUE(is_constant(res.expression()));
+  EXPECT_EQ(get_constant_value(res.expression()), a_val_ - b_val_ - c_val_);
+}
