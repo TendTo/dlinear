6b58da07c39f17ea32ca784c85c69debfdc0fcd9
Ernesto Casablanca <casablancaernesto@gmail.com>
2024-11-11 18:00:00 +0000

    docs: fix icon in title

--- docs/filters/markdown.sh
+++ docs/filters/markdown.sh
@@ -12,11 +12,14 @@ readonly regex_substitute_math='\2\\f$\3'
 
 # KNOWN LIMITATION: breaks if the character ` is used in the mermaid code
 readonly regex_match_mermaid='```mermaid\n([^\`]*)```'
-readonly regex_substitute_mermaid='<pre class='mermaid'>\n\1<\/pre>'
+readonly regex_substitute_mermaid="<pre class='mermaid'>\n\1<\/pre>"
+
+readonly regex_title_logo='<img alt="Icon" src="docs\/_static\/logo.svg" align="left" width="35" height="35">'
 
 cat "${1}" \
 | sed -E \
     -e "s/$regex_match_math_split/$regex_substitute_math_split/g" \
     -e "s/$regex_match_math/$regex_substitute_math/g" \
+    -e "s/$regex_title_logo//g" \
 | sed -E -z  \
     -e "s/$regex_match_mermaid/$regex_substitute_mermaid/g"
